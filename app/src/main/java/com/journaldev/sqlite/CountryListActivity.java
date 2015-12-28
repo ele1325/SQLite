2b7da1154aa5512fef84469b63d148c2d4b86490
Jimmy Lee <ele1325@hotmail.com>
2015-12-28 22:35:18 +0800

    20151228
    AdMob

--- app/src/main/java/com/journaldev/sqlite/CountryListActivity.java
+++ app/src/main/java/com/journaldev/sqlite/CountryListActivity.java
@@ -15,6 +15,8 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.TextView;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -39,6 +41,10 @@ public class CountryListActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.fragment_emp_list);
+        AdView mAdView = (AdView) findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        mAdView.loadAd(adRequest);
+
         mToolbar = (Toolbar) findViewById(R.id.app_bar);
         setSupportActionBar(mToolbar);
         dbManager = new DBManager(this);
@@ -52,36 +58,6 @@ public class CountryListActivity extends AppCompatActivity {
         LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
         //linearLayoutManager.setOrientation(LinearLayoutManager.HORIZONTAL);
         recyclerView.setLayoutManager(linearLayoutManager);
-
-//        listView = (ListView) findViewById(R.id.list_view);
-//        listView.setEmptyView(findViewById(R.id.empty));
-//        adapter = new SimpleCursorAdapter(this, R.layout.activity_view_record, cursor, from, to, 0);
-//        adapter.notifyDataSetChanged();
-//        listView.setAdapter(adapter);
-
-        // OnCLickListiner For List Items
-//        recyclerView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-//            @Override
-//            public void onItemClick(AdapterView<?> parent, View view, int position, long viewId) {
-//                TextView idTextView = (TextView) view.findViewById(R.id.id);
-//                TextView textView1 = (TextView) view.findViewById(R.id.textView1);
-//                TextView textView2 = (TextView) view.findViewById(R.id.textView2);
-//                TextView dateTextView = (TextView) view.findViewById(R.id.date);
-//
-//                String id = idTextView.getText().toString();
-//                String item1 = textView1.getText().toString();
-//                String item2 = textView2.getText().toString();
-//                String date = dateTextView.getText().toString();
-//
-//               Intent modify_intent = new Intent(getApplicationContext(), ModifyCountryActivity.class);
-//                modify_intent.putExtra("id", id);
-//                modify_intent.putExtra("item1", item1);
-//                modify_intent.putExtra("item2", item2);
-//                modify_intent.putExtra("date", date);
-//
-//                startActivity(modify_intent);
-//            }
-//        });
     }
 
     public static List<Information> getData(){
